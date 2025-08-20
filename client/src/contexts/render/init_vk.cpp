10792b09f3f3f8f79d77210f9e0af0c8206bbdb7
niooi <onioniwonpounoin@gmail.com>
2025-08-19 20:06:38 -0400

    update v.sh

--- client/src/contexts/render/init_vk.cpp
+++ client/src/contexts/render/init_vk.cpp
@@ -32,14 +32,14 @@ namespace v {
 
         // Create daxa instance
         instance =
-            daxa::create_instance({ .engine_name = "vengine", .app_name = "vengine" });
+            daxa::create_instance({ .engine_name = "vengine", .app_name = "vengine", });
 
         LOG_DEBUG("Daxa instance created successfully");
 
         daxa::DeviceInfo2 device_info = {
             .max_allowed_buffers = 16384,
             .max_allowed_images  = 16384,
-            .name                = "vengine_device",
+            .name                = "vengine_gpu",
         };
 
         // Let daxa choose a suitable device
@@ -67,20 +67,8 @@ namespace v {
                 .name = "pipeline manager",
             });
 
-        LOG_INFO("Daxa context initialized successfully");
+        LOG_INFO("daxa + vulkan stuff initialized");
     }
 
-    DaxaResources::~DaxaResources()
-    {
-        LOG_INFO("Cleaning up DaxaResources...");
-
-        // Daxa handles cleanup automatically through RAII
-        if (device.is_valid())
-        {
-            device.wait_idle();
-            device.collect_garbage();
-        }
-
-        LOG_INFO("DaxaResources cleanup complete");
-    }
+    DaxaResources::~DaxaResources() { LOG_INFO("Cleaning up daxa + vulkan stuff..."); }
 } // namespace v
