4e55169d4d4e231b1ce6e4a1eb88c94b839670c0
niooii <onioniwonpounoin@gmail.com>
2025-08-20 14:56:18 -0400

    scaffold hdag

---
+++ common/include/containers/hashdag.h
@@ -0,0 +1,22 @@
+#include <defs.h>
+
+namespace v {
+    /// A Hashmap + Sparse Directed Acyclic Graph structure based on
+    /// https://github.com/Phyronnaz/HashDAG
+    /// Paper: https://onlinelibrary.wiley.com/doi/full/10.1111/cgf.13916
+
+
+    template <typename T>
+    struct HDAGNode {
+        T val;
+    };
+
+    template <typename T>
+    class HDAG {
+    public:
+        // rough scaffold for now
+        void insert();
+    private:
+        
+    };
+} // namespace v
