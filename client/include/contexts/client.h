bbb571aba66c06eb0e1a2588e41d14dc34e08bf2
niooi <onioniwonpounoin@gmail.com>
2025-09-26 22:55:39 -0400

    use vcpkg cache

---
+++ client/include/contexts/client.h
@@ -0,0 +1,14 @@
+//
+// Created by niooi on 9/26/2025.
+//
+
+#pragma once
+
+#include <prelude.h>
+#include "domain/context.h"
+
+namespace v {
+    class ClientContext : public Context {
+
+    };
+};
