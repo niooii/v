8685220e2c2b5a7aee7f856ec5d23e15b2a69ddb
niooii <onioniwonpounoin@gmail.com>
2025-12-19 23:50:42 -0500

    checkpoint

--- common/include/mem/ref_counted.h
+++ common/include/mem/ref_counted.h
@@ -8,17 +8,36 @@
 #include <memory>
 
 // TODO!
-
 namespace v::mem {
     /// A reference counted smart pointer, not atomic to avoid unneeded overhead
-    /// TODO! also what is an intrusive pointer lol that sounds useufl
     template <typename T>
     class Rc {
     public:
+        // actual constructors
+        template <typename... Args>
+        Rc(Args&&... args);
+        // TODO add a value constructor and ref and double ref (move)
+
+        // copy constructors
+        Rc(const Rc&) { blk_->refc++; };
+        Rc& operator=(const Rc&) { blk_->refc++; };
+
+        // move constructors
+        Rc(Rc&&)            = default;
+        Rc& operator=(Rc&&) = default;
+
+        ~Rc()
+        {
+            blk_->refc--;
+
+            if (!blk_->refc)
+                delete ptr_;
+        }
+
     private:
         struct ControlBlock {
-            u64 refc_;
-        } blk_;
+            u64 refc;
+        }* blk_;
 
         T* ptr_;
     };
@@ -32,3 +51,12 @@ namespace v::mem {
         std::shared_ptr<T> ptr_;
     };
 } // namespace v::mem
+
+// aliases for now
+namespace v {
+    template <typename T>
+    using Rc = v::mem::Rc<T>;
+
+    template <typename T>
+    using Arc = v::mem::Rc<T>;
+} // namespace v
